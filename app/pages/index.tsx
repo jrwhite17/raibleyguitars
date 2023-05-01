17723e41f2d84452be06a4468b606f2b3fba5d04
Johnathan White <johnrileywhite@gmail.com>
2023-05-01 17:44:05 -0500

    setup firebase

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,11 +1,32 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 import styles from '../styles/Home.module.css';
 
 //Import flat file of products
 import products from '../products.json'
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
