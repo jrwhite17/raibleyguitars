1b8001ac4984a607ba1fe86711059316491b9ec9
Johnathan White <johnrileywhite@gmail.com>
2023-05-01 18:03:43 -0500

    fixing working directory

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -17,7 +17,7 @@ import products from '../products.json'
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.projectId,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
