e4a6f1aecf434edef4408782ebe20922d749c1ab
Johnathan White <johnrileywhite@gmail.com>
2023-05-02 17:08:37 -0500

    added productId page

---
+++ app/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import Container from '../Container';
+
+import styles from './Footer.module.css';
+
+const Footer = ({ children }) => {
+  return (
+    <footer className={styles.footer}>
+      <Container className={styles.footerContainer}>
+        &copy; White Computing
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer;
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,3 +1,8 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+import Layout from '../components/Layout';
+import Container from '../components/Container';
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 // Import the functions you need from the SDKs you need
@@ -33,7 +38,6 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      
       {/*Raibley Guitars Logo*/}
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
@@ -47,29 +51,37 @@ export default function Home() {
       </div>
 
       {/*Products*/}
+      <Container className={styles.homeContainer}>
       <div className={styles.grid}>
         {products.map(product => {
           return (
             <div key={product.id} className={styles.card}>
-              <img src={product.image} alt={`Preview of ${product.title}`} />
-              <h3>{ product.title }</h3>
-              <p>{ product.description }</p>
-              <p>${ product.price }</p>
+              <Link href={`/products/${product.id}`}>
+                  <img src={product.image} alt={`Preview of ${product.title}`} />
+                  <h3>{ product.title }</h3>
+                  <p>{ product.description }</p>
+                  <p>${ product.price }</p>
+              </Link>
               <p>
-              <button className="snipcart-add-item"
-                data-item-id={product.id}
-                data-item-image={product.image}
-                data-item-name={product.title}
-                data-item-price={product.price}
-                >
-                Add to Cart
-              </button>
+                <button className="snipcart-add-item"
+                  data-item-id={product.id}
+                  data-item-image={product.image}
+                  data-item-name={product.title}
+                  data-item-price={product.price}
+                  >
+                  Add to Cart
+                </button>
               </p>
             </div>
           );
         })}
       </div>
-
+      </Container>
+      <footer className={styles.footer}>
+      <Container className={styles.footerContainer}>
+        &copy; White Computing
+      </Container>
+    </footer>
     </main>
   )
 }
