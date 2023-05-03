fc246a33afa55840103278ec5f7c1dc41b991251
Johnathan White <johnrileywhite@gmail.com>
2023-05-02 19:32:18 -0500

    editing css and logo

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 import Link from 'next/link';
-
+import Image from 'next/image'
 import Layout from '../components/Layout';
 import Container from '../components/Container';
-import Image from 'next/image'
+
 import { Inter } from 'next/font/google'
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -38,16 +38,14 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      {/*Raibley Guitars Logo*/}
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/logo.png"
-          alt="Raibley Guitars Logo"
-          width={360}
-          height={37}
-          priority
-        />
+      <Head>
+        <title>Raibley Guitars</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      {/* Logo */}
+      <div className={styles.logo}>
+        <Image src="/logo.png" alt="Logo" width={250} height={250} />
       </div>
 
       {/*Products*/}
